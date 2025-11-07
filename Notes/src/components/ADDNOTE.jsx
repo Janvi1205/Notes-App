a8d8ecef426ccb57f890d6ccf42945603e7e6165
Janvi1205 <janviyadav120505@gmail.com>
2025-11-07 12:16:34 +0530

    Added two-way-binding concept

--- Notes/src/components/ADDNOTE.jsx
+++ Notes/src/components/ADDNOTE.jsx
@@ -1,15 +1,33 @@
-const ADDNOTE =()=>{
-    return(
+import { useState } from "react";
+
+const ADDNOTE = () => {
+    const [title, settitle] = useState("")
+    const [detail, setdetail] = useState("")
+
+    function handlesubmit(e)
+    {
+        e.preventDefault();
+        console.log(title, detail);
+
+    }
+    return (
         <div>
             <div className="fixed flex inset-0  items-center justify-center bg-black/50 z-50">
-               <div className="bg-white p-6 rounded-2xl w-[500px] h-120 ">
-                <h1 className="flex justify-center font-bold text-2xl">Add a Note</h1>
-                 <form className=" flex flex-col mt-10 gap-10" action="">
-                    <input className="border-2 rounded-lg p-4" type="text" placeholder="Enter the Title"  id="" />
-                    <textarea className="border-2 h-55 rounded-lg p-4" placeholder="Enter the note" id=""></textarea>
+                <div className="bg-white p-6 rounded-2xl w-[500px] h-140 ">
+                    <h1 className="flex justify-center font-bold text-2xl">Add a Note</h1>
+                    <form onSubmit={(e)=>handlesubmit(e)}  className=" flex flex-col mt-10 gap-10" action="">
+                        <input
+                            value={title}
+                            onChange={(e) => { settitle(e.target.value) }}
+                            className="border-2 rounded-lg p-4" type="text" placeholder="Enter the Title" id="" />
+                        <textarea
+                            value={detail}
+                            onChange={(e) => { setdetail(e.target.value) }}
+                            className="  border-2 h-80 rounded-lg p-4" placeholder="Enter the note" id=""></textarea>
 
-                </form>
-               </div>
+                        <button className="-mt-8 rounded-2xl bg-amber-950 text-white p-1">AddIt</button>    
+                    </form>
+                </div>
 
             </div>
         </div>
